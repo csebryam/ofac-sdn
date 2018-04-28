78cee5a3d1ff041c67f7cc1bc30a615d91cf6246
Bryam Noguera <soccerbryam@yahoo.com>
2018-04-28 01:44:09 -0700

    add usage OFAC tests

--- test/ofac_sdn_test.rb
+++ test/ofac_sdn_test.rb
@@ -2,10 +2,20 @@ require "test_helper"
 
 class OfacSdnTest < Minitest::Test
   def test_that_it_has_a_version_number
-    refute_nil ::OfacSdn::VERSION
+    refute_nil OfacSdn::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_can_get_publish_date
+    refute_nil OfacSdn::Read.publish_date
+  end
+
+  def test_can_get_record_count
+    refute_nil OfacSdn::Read.record_count
+  end
+
+  def test_can_output_sdn_hash
+    hash = OfacSdn::Read.call
+    refute_nil hash.keys
+    assert_equal(hash.keys.count, 10)
   end
 end
