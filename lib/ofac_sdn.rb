fe7792fdb3a6a5f8c97fb94888c2886678e3d170
Bryam Noguera <soccerbryam@yahoo.com>
2018-04-28 01:43:50 -0700

    add main ofac sdn

--- lib/ofac_sdn.rb
+++ lib/ofac_sdn.rb
@@ -1,5 +1,34 @@
-require "ofac_sdn/version"
+# encoding: utf-8
+# frozen_string_literal: true
+
+require 'ofac_sdn/document'
+require 'ofac_sdn/parser'
+require 'ofac_sdn/version'
+require 'ofac_sdn/xsd'
 
 module OfacSdn
-  # Your code goes here...
+  # OfacSdn::Read.new creates the SDN Hash document
+  class Read
+    class << self
+      def call
+        new.call(document)
+      end
+
+      def document
+        @document ||= Document.call
+      end
+
+      def publish_date
+        document.css('sdnList publshInformation Publish_Date').text
+      end
+
+      def record_count
+        document.css('sdnList publshInformation Record_Count').text.to_i
+      end
+    end
+
+    def call(document)
+      Parser.run(document: document, xsd: XSD_HASH)
+    end
+  end
 end
